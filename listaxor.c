e4f85c78a7bf2735aa6e3ad890e59fdd10ebc347
Angel Eduardo <aepatino.360@gmail.com>
2023-01-07 21:36:46 -0400

    Agregada la estructura del proyecto, typedefs creados para nodos y listas junto a la funcion para XOR.

--- listaxor.c
+++ listaxor.c
@@ -2,3 +2,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "listaxor.h"
+
+// Funcion para hacer XOR entre direcciones de punteros (Usando la libreria <stdint.h>)
+
+node *XOR(node *x, node *y)
+{
+    return (node*)((uintptr_t)(x) ^ (uintptr_t)(y));
+}
--- listaxor.h
+++ listaxor.h
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
+// Definicion de las estructuras para la lista y los nodos
+
+typedef struct Node
+{
+    int data;
+    Node *prev_next;
+}node;
+
+typedef struct List
+{
+    node *head, *tail;
+}list;
+
+// Funcion para hacer XOR entre direcciones de punteros (Usando la libreria <stdint.h>)
+
+node *XOR(node *x, node *y);
